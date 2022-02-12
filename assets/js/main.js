1e3c638ee88cf3e8a21befca9b644c5250419e82
nagiyevel <elchin@nagiyev.pro>
2022-02-12 16:50:17 +0400

    Added project filters by category

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,25 +1,23 @@
-jQuery(document).ready(function($) {
-
-
+jQuery(document).ready(function ($) {
+    $('.project_filter').click(function (e) {
+        e.preventDefault();
+        let class_name = $(this).attr('href').substring(1);
+        if (class_name) {
+            class_name = '.' + class_name;
+            $('.projects-section .item' + class_name).show();
+            $('.projects-section .item:not(' + class_name+')').hide();
+        }else{
+            $('.projects-section .item').show();
+        }
+    })
     /*======= Skillset *=======*/
-    
-    
     $('.level-bar-inner').css('width', '0');
-    
-    $(window).on('load', function() {
-
-        $('.level-bar-inner').each(function() {
-        
+    $(window).on('load', function () {
+        $('.level-bar-inner').each(function () {
             var itemWidth = $(this).data('level');
-            
             $(this).animate({
                 width: itemWidth
             }, 800);
-            
         });
-
     });
-   
-    
-
 });
